7de497ac13bdd98e303df30a16eeea9c19501a75
dediwibisono17 <dedi.wibisono17@gmail.com>
2025-05-23 13:55:48 +0700

    deded

--- js/app.js
+++ js/app.js
@@ -49,3 +49,54 @@ $('.slider-alur').slick({
 
 
 
+
+
+//gaji
+
+gsap.timeline({
+    scrollTrigger: {
+        trigger: ".boxx",
+        start: "center center",
+        end: "bottom top",
+        scrub: 1,
+        pin: true
+    }
+})
+    .from(".textx", {
+        x: innerWidth * 1
+    })
+    .from(".texty", {
+        x: innerWidth * -1
+    })
+    .from(".textz", {
+        x: innerWidth * 1
+    })
+    .from(".texta", {
+        x: innerWidth * -1
+    })
+// .from(".logo", {
+//     y: innerHeight * 1,
+//     rotate: 360
+// })
+
+
+gsap.timeline({
+    scrollTrigger: {
+        trigger: ".box2",
+        start: "center center",
+        end: "bottom top",
+        scrub: 1,
+        pin: true
+    }
+})
+    .from(".box2", {
+        opacity: 0,
+    })
+    .from(".text4", {
+        y: innerHeight * 1,
+        stagger: {
+            amount: 0.5,
+        }
+    })
+
+
